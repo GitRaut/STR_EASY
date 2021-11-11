2470a25fff9577fe1b7b578a59571aafab05a118
Mango <92181249+GitRaut@users.noreply.github.com>
2021-11-11 11:38:07 +0300

    Update itc_even_place.cpp

--- itc_even_place.cpp
+++ itc_even_place.cpp
@@ -3,8 +3,11 @@
 string itc_even_place(string str){
     long long len = itc_len(str);
     string result;
+    if(itc_len(str) <= 1){
+        return "-1";
+    }
     for(int i = 0; i < len; i++){
-        if(i % 2 == 0){
+        if(i % 2 == 1){
             result = result + str[i];
         }
     }
