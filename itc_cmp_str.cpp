200d2f66b28ef0781fe0f367d67fef18a651726a
Mango <92181249+GitRaut@users.noreply.github.com>
2021-11-11 12:10:37 +0300

    Update itc_cmp_str.cpp

--- itc_cmp_str.cpp
+++ itc_cmp_str.cpp
@@ -3,7 +3,6 @@
 string itc_cmp_str(string str1, string str2, int num){
     string result = "";
     long long len1 = itc_len(str1), len2 = itc_len(str2), count = 0;
-    int j = 0;
     if(str2 == ""){
         return str1;
     }
@@ -16,10 +15,10 @@ string itc_cmp_str(string str1, string str2, int num){
     for(int i = 0; i < len2 && i < len1; i++){
         result = result + str2[i];
     }
+    int j = len1 - itc_len(result);
     for(int i = itc_len(result); i < len1; i++){
-        result = result + str1[j + 1];
+        result = result + str1[j];
         j++;
     }
-
     return itc_slice_str(result, 0, len1-1);
 }
